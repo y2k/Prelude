a7614fe18237d942673e874e630852f60da27476
y2k <itwisterlx@gmail.com>
2019-10-08 19:02:59 +0300

    refactoring

--- Library.fs
+++ Library.fs
@@ -1,14 +1,13 @@
-﻿namespace Prelude
+﻿[<AutoOpen>]
+module Prelude
 
-[<AutoOpen>]
-module CommonUtils =
-    let inline (^) f x = f x
+let inline (^) f x = f x
 
-    let inline (<!) f a () = f a
-    let inline (>>=) ma mf = async.Bind(ma, mf)
-    let inline (>>-) ma f = async.Bind(ma, f >> async.Return)
+let inline (<!) f a () = f a
+let inline (>>=) ma mf = async.Bind(ma, mf)
+let inline (>>-) ma f = async.Bind(ma, f >> async.Return)
 
-    let inline always a _ = a
-    let inline flip f a b = f b a
-    let inline curry f a b = f (a, b)
-    let inline uncurry f (a, b) = f a b
+let inline always a _ = a
+let inline flip f a b = f b a
+let inline curry f a b = f (a, b)
+let inline uncurry f (a, b) = f a b
