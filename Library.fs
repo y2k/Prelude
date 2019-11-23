3b3bb8a90719e419e099ad46fd95b78cf9b1004a
y2k <itwisterlx@gmail.com>
2019-11-23 17:10:33 +0300

    add >>-! operator

--- Library.fs
+++ Library.fs
@@ -6,6 +6,7 @@ let inline (^) f x = f x
 let inline (<!) f a () = f a
 let inline (>>=) ma mf = async.Bind(ma, mf)
 let inline (>>-) ma f = async.Bind(ma, f >> async.Return)
+let inline (>>-!) t f = (Async.AwaitTask t) >>- f
 
 let inline always a _ = a
 let inline flip f a b = f b a
