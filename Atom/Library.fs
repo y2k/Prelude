b4586370f8fd5fde5c8f8d825274e8d8b9674ab8
y2k <itwisterlx@gmail.com>
2019-11-24 17:23:21 +0300

    add atom project

---
+++ Atom/Library.fs
@@ -0,0 +1,17 @@
+﻿module Atom
+
+type IAtom<'T when 'T : not struct>(value : 'T) =
+    let refCell = ref value
+    
+    let rec swap' f = 
+        let currentValue = !refCell
+        let result = System.Threading.Interlocked.CompareExchange<'T>(refCell, f currentValue, currentValue)
+        if obj.ReferenceEquals(result, currentValue) then result
+        else System.Threading.Thread.SpinWait 20; swap' f
+
+    member __.Value with get() = !refCell
+    member __.swap (f : 'T -> 'T) = swap' f
+    member this.update f = this.swap f |> ignore
+    member this.dispatch f = this.swap (f >> fst) |> (f >> snd)
+
+let atom value = new IAtom<_>(value)
