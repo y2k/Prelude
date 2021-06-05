30ae683b769e0d422c56a82be35ea57be3fef97c
y2k <itwisterlx@gmail.com>
2021-06-05 22:18:13 +0300

    format code

--- Core/Library.fs
+++ Core/Library.fs
@@ -16,19 +16,32 @@ let inline flip f a b = f b a
 let inline curry f a b = f (a, b)
 let inline uncurry f (a, b) = f a b
 
-let inline (!>) (x: ^a): ^b =
-    ((^a or ^b): (static member op_Implicit: ^a -> ^b) x)
+let inline (!>) (x: ^a) : ^b =
+    ((^a or ^b): (static member op_Implicit : ^a -> ^b) x)
 
-let [<Obsolete>] TODO() = raise ^ NotImplementedException()
+[<Obsolete>]
+let TODO () = raise ^ NotImplementedException()
 
 let (|Regex|_|) pattern input =
-    let m = Text.RegularExpressions.Regex.Match(input, pattern)
-    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
-    else None
+    let m =
+        Text.RegularExpressions.Regex.Match(input, pattern)
+
+    if m.Success then
+        Some(List.tail [ for g in m.Groups -> g.Value ])
+    else
+        None
 
 module Result =
-    let inline unwrap r = match r with | Ok x -> x | Error e -> failwith e
-    let inline wrap f = try f () |> Ok with e -> Error e
+    let inline unwrap r =
+        match r with
+        | Ok x -> x
+        | Error e -> failwith e
+
+    let inline wrap f =
+        try
+            f () |> Ok
+        with e -> Error e
+
     let inline fold fok ferror =
         function
         | Ok x -> fok x
@@ -36,18 +49,24 @@ module Result =
 
 module Async =
     let inline map f a = async.Bind(a, f >> async.Return)
-    let inline catch a = a |> Async.Catch |> map (function Choice1Of2 x -> Ok x | Choice2Of2 e -> Error e)
+
+    let inline catch a =
+        a
+        |> Async.Catch
+        |> map
+            (function
+            | Choice1Of2 x -> Ok x
+            | Choice2Of2 e -> Error e)
 
 module List =
     let inline isNotEmpty xs = List.isEmpty xs |> not
 
 module String =
-    let inline split (x : String) (separator : Char) = x.Split separator
+    let inline split (x: String) (separator: Char) = x.Split separator
 
-type Microsoft.FSharp.Control.AsyncBuilder with
-    member __.Bind (t : Threading.Tasks.Task<'T>, f:'T -> Async<'R>) : Async<'R> =
-        async.Bind(Async.AwaitTask t, f)
-    member __.ReturnFrom (t : Threading.Tasks.Task<'T>) : Async<'T> =
-        async.ReturnFrom(Async.AwaitTask t)
-    member __.Bind (t : Threading.Tasks.ValueTask<'T>, f:'T -> Async<'R>) : Async<'R> =
+type Control.AsyncBuilder with
+    member __.Bind(t: Threading.Tasks.Task<'T>, f: 'T -> Async<'R>) : Async<'R> = async.Bind(Async.AwaitTask t, f)
+    member __.ReturnFrom(t: Threading.Tasks.Task<'T>) : Async<'T> = async.ReturnFrom(Async.AwaitTask t)
+
+    member __.Bind(t: Threading.Tasks.ValueTask<'T>, f: 'T -> Async<'R>) : Async<'R> =
         async.Bind(Async.AwaitTask <| t.AsTask(), f)
