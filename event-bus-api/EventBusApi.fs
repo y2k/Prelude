b6b2055e903a124b13b5064b6b445de3914bf719
y2k <itwisterlx@gmail.com>
2023-02-19 17:47:08 +0300

    add event bus

---
+++ event-bus-api/EventBusApi.fs
@@ -0,0 +1,17 @@
+﻿namespace Y2k.EventBus
+
+type Event =
+    interface
+    end
+
+type Command =
+    interface
+    end
+
+type Initialize = Initialize
+    with
+        interface Event
+
+type InitializeCompleted = InitializeCompleted
+    with
+        interface Command
